d634b9bb3195ecfdaf24c044b19dade46da97378
Steve <trisoft@abv.bg>
2016-11-09 15:34:05 +0200

    Added Ex. 6. - 2D graphics

--- ex5/main.cpp
+++ ex5/main.cpp
@@ -1,239 +1,167 @@
-/** Example 001 HelloWorld
+/** Example 006 2D Graphics
 
-This Tutorial shows how to set up the IDE for using the Irrlicht Engine and how
-to write a simple HelloWorld program with it. The program will show how to use
-the basics of the VideoDriver, the GUIEnvironment, and the SceneManager.
-Microsoft Visual Studio is used as an IDE, but you will also be able to
-understand everything if you are using a different one or even another
-operating system than windows.
+This Tutorial shows how to do 2d graphics with the Irrlicht Engine.
+It shows how to draw images, keycolor based sprites,
+transparent rectangles, and different fonts. You may consider
+this useful if you want to make a 2d game with the engine, or if
+you want to draw a cool interface or head up display for your 3d game.
 
-You have to include the header file <irrlicht.h> in order to use the engine. The
-header file can be found in the Irrlicht Engine SDK directory \c include. To let
-the compiler find this header file, the directory where it is located has to be
-specified. This is different for every IDE and compiler you use. Let's explain
-shortly how to do this in Microsoft Visual Studio:
-
-- If you use Version 6.0, select the Menu Extras -> Options.
-  Select the directories tab, and select the 'Include' Item in the combo box.
-  Add the \c include directory of the irrlicht engine folder to the list of
-  directories. Now the compiler will find the Irrlicht.h header file. We also
-  need the irrlicht.lib to be found, so stay in that dialog, select 'Libraries'
-  in the combo box and add the \c lib/VisualStudio directory.
-  \image html "vc6optionsdir.jpg"
-  \image latex "vc6optionsdir.jpg"
-  \image html "vc6include.jpg"
-  \image latex "vc6include.jpg"
-
-- If your IDE is Visual Studio .NET, select Tools -> Options.
-  Select the projects entry and then select VC++ directories. Select 'show
-  directories for include files' in the combo box, and add the \c include
-  directory of the irrlicht engine folder to the list of directories. Now the
-  compiler will find the Irrlicht.h header file. We also need the irrlicht.lib
-  to be found, so stay in that dialog, select 'show directories for Library
-  files' and add the \c lib/VisualStudio directory.
-  \image html "vcnetinclude.jpg"
-  \image latex "vcnetinclude.jpg"
-
-That's it. With your IDE set up like this, you will now be able to develop
-applications with the Irrlicht Engine.
-
-Lets start!
-
-After we have set up the IDE, the compiler will know where to find the Irrlicht
-Engine header files so we can include it now in our code.
+As always, I include the header files, use the irr namespace,
+and tell the linker to link with the .lib file.
 */
 #include <irrlicht.h>
+#include "driverChoice.h"
 
-/*
-In the Irrlicht Engine, everything can be found in the namespace 'irr'. So if
-you want to use a class of the engine, you have to write irr:: before the name
-of the class. For example to use the IrrlichtDevice write: irr::IrrlichtDevice.
-To get rid of the irr:: in front of the name of every class, we tell the
-compiler that we use that namespace from now on, and we will not have to write
-irr:: anymore.
-*/
 using namespace irr;
 
-/*
-There are 5 sub namespaces in the Irrlicht Engine. Take a look at them, you can
-read a detailed description of them in the documentation by clicking on the top
-menu item 'Namespace List' or by using this link:
-http://irrlicht.sourceforge.net/docu/namespaces.html
-Like the irr namespace, we do not want these 5 sub namespaces now, to keep this
-example simple. Hence, we tell the compiler again that we do not want always to
-write their names.
-*/
-using namespace core;
-using namespace scene;
-using namespace video;
-using namespace io;
-using namespace gui;
-
-/*
-To be able to use the Irrlicht.DLL file, we need to link with the Irrlicht.lib.
-We could set this option in the project settings, but to make it easy, we use a
-pragma comment lib for VisualStudio. On Windows platforms, we have to get rid
-of the console window, which pops up when starting a program with main(). This
-is done by the second pragma. We could also use the WinMain method, though
-losing platform independence then.
-*/
 #ifdef _IRR_WINDOWS_
 #pragma comment(lib, "Irrlicht.lib")
 #pragma comment(linker, "/subsystem:windows /ENTRY:mainCRTStartup")
 #endif
 
-
 /*
-This is the main method. We can now use main() on every platform.
+At first, we let the user select the driver type, then start up the engine, set
+a caption, and get a pointer to the video driver.
 */
 int main()
 {
-	/*
-	The most important function of the engine is the createDevice()
-	function. The IrrlichtDevice is created by it, which is the root
-	object for doing anything with the engine. createDevice() has 7
-	parameters:
+	// ask user for driver
+	/*video::E_DRIVER_TYPE driverType=driverChoiceConsole();
+	if (driverType==video::EDT_COUNT)
+		return 1;*/
 
-	- deviceType: Type of the device. This can currently be the Null-device,
-	   one of the two software renderers, D3D8, D3D9, or OpenGL. In this
-	   example we use EDT_SOFTWARE, but to try out, you might want to
-	   change it to EDT_BURNINGSVIDEO, EDT_NULL, EDT_DIRECT3D8,
-	   EDT_DIRECT3D9, or EDT_OPENGL.
+	// create device
 
-	- windowSize: Size of the Window or screen in FullScreenMode to be
-	   created. In this example we use 640x480.
+	IrrlichtDevice *device = createDevice(video::EDT_DIRECT3D9,
+		core::dimension2d<u32>(800, 600));
 
-	- bits: Amount of color bits per pixel. This should be 16 or 32. The
-	   parameter is often ignored when running in windowed mode.
+	if (device == 0)
+		return 1; // could not create selected driver.
 
-	- fullscreen: Specifies if we want the device to run in fullscreen mode
-	   or not.
+	device->setWindowCaption(L"Irrlicht Engine - 2D Graphics Demo");
 
-	- stencilbuffer: Specifies if we want to use the stencil buffer (for
-	   drawing shadows).
-
-	- vsync: Specifies if we want to have vsync enabled, this is only useful
-	   in fullscreen mode.
-
-	- eventReceiver: An object to receive events. We do not want to use this
-	   parameter here, and set it to 0.
-
-	Always check the return value to cope with unsupported drivers,
-	dimensions, etc.
-	*/
-	IrrlichtDevice *device =
-		createDevice( video::EDT_SOFTWARE, dimension2d<u32>(1440, 900), 32,
-			1, 1, false, 0);
-
-	if (!device)
-		return 1;
+	video::IVideoDriver* driver = device->getVideoDriver();
 
 	/*
-	Set the caption of the window to some nice text. Note that there is an
-	'L' in front of the string. The Irrlicht Engine uses wide character
-	strings when displaying text.
+	All 2d graphics in this example are put together into one texture,
+	2ddemo.png. Because we want to draw colorkey based sprites, we need to
+	load this texture and tell the engine, which part of it should be
+	transparent based on a colorkey.
+
+	In this example, we don't tell it the color directly, we just say "Hey
+	Irrlicht Engine, you'll find the color I want at position (0,0) on the
+	texture.". Instead, it would be also possible to call
+	driver->makeColorKeyTexture(images, video::SColor(0,0,0,0)), to make
+	e.g. all black pixels transparent. Please note that
+	makeColorKeyTexture just creates an alpha channel based on the color.
 	*/
-	device->setWindowCaption(L"Hello World! - Irrlicht Engine Demo!!!");
+	video::ITexture* images = driver->getTexture("./media/2ddemo.png");
+	driver->makeColorKeyTexture(images, core::position2d<s32>(0,0));
 
 	/*
-	Get a pointer to the VideoDriver, the SceneManager and the graphical
-	user interface environment, so that we do not always have to write
-	device->getVideoDriver(), device->getSceneManager(), or
-	device->getGUIEnvironment().
+	To be able to draw some text with two different fonts, we first load
+	them. Ok, we load just one. As the first font we just use the default
+	font which is built into the engine. Also, we define two rectangles
+	which specify the position of the images of the red imps (little flying
+	creatures) in the texture.
 	*/
-	IVideoDriver* driver = device->getVideoDriver();
-	ISceneManager* smgr = device->getSceneManager();
-	IGUIEnvironment* guienv = device->getGUIEnvironment();
+	gui::IGUIFont* font = device->getGUIEnvironment()->getBuiltInFont();
+	gui::IGUIFont* font2 =
+		device->getGUIEnvironment()->getFont("./media/fonthaettenschweiler.bmp");
+
+	core::rect<s32> imp1(349,15,385,78);
+	core::rect<s32> imp2(387,15,423,78);
 
 	/*
-	We add a hello world label to the window, using the GUI environment.
-	The text is placed at the position (10,10) as top left corner and
-	(260,22) as lower right corner.
+	Prepare a nicely filtering 2d render mode for special cases.
 	*/
-	guienv->addStaticText(L"Hello World! This is the Irrlicht Software renderer!",
-		rect<s32>(10,10,260,22), true);
+	driver->getMaterial2D().TextureLayer[0].BilinearFilter=true;
+	driver->getMaterial2D().AntiAliasing=video::EAAM_FULL_BASIC;
 
 	/*
-	To show something interesting, we load a Quake 2 model and display it.
-	We only have to get the Mesh from the Scene Manager with getMesh() and add
-	a SceneNode to display the mesh with addAnimatedMeshSceneNode(). We
-	check the return value of getMesh() to become aware of loading problems
-	and other errors.
-
-	Instead of writing the filename sydney.md2, it would also be possible
-	to load a Maya object file (.obj), a complete Quake3 map (.bsp) or any
-	other supported file format. By the way, that cool Quake 2 model
-	called sydney was modelled by Brian Collins.
+	Everything is prepared, now we can draw everything in the draw loop,
+	between the begin scene and end scene calls. In this example, we are
+	just doing 2d graphics, but it would be no problem to mix them with 3d
+	graphics. Just try it out, and draw some 3d vertices or set up a scene
+	with the scene manager and draw it.
 	*/
-	IAnimatedMesh* mesh = smgr->getMesh("./media/sydney.md2");
-	if (!mesh)
+	while(device->run() && driver)
 	{
-		device->drop();
-		return 1;
+		if (device->isWindowActive())
+		{
+			u32 time = device->getTimer()->getTime();
+
+			driver->beginScene(true, true, video::SColor(255,120,102,136));
+
+			/*
+			First, we draw 3 sprites, using the alpha channel we
+			created with makeColorKeyTexture. The last parameter
+			specifies that the drawing method should use this alpha
+			channel. The last-but-one parameter specifies a
+			color, with which the sprite should be colored.
+			(255,255,255,255) is full white, so the sprite will
+			look like the original. The third sprite is drawn
+			with the red channel modulated based on the time.
+			*/
+
+			// draw fire & dragons background world
+			driver->draw2DImage(images, core::position2d<s32>(50,50),
+				core::rect<s32>(0,0,342,224), 0,
+				video::SColor(255,255,255,255), true);
+
+			// draw flying imp
+			driver->draw2DImage(images, core::position2d<s32>(164,125),
+				(time/500 % 2) ? imp1 : imp2, 0,
+				video::SColor(255,255,255,255), true);
+
+			// draw second flying imp with colorcylce
+			driver->draw2DImage(images, core::position2d<s32>(270,105),
+				(time/500 % 2) ? imp1 : imp2, 0,
+				video::SColor(255,255,255,255), true);
+
+			/*
+			Drawing text is really simple. The code should be self
+			explanatory.
+			*/
+
+			// draw some text
+			if (font)
+				font->draw(L"This demo shows that Irrlicht is also capable of drawing 2D graphics.",
+					core::rect<s32>(130,10,300,50),
+					video::SColor(255,255,255,255));
+
+			// draw some other text
+			if (font2)
+				font2->draw(L"Also mixing with 3d graphics is possible.",
+					core::rect<s32>(130,20,300,60),
+					video::SColor(255,255,255,255));
+
+			/*
+			Next, we draw the Irrlicht Engine logo (without
+			using a color or an alpha channel). Since we slightly scale
+			the image we use the prepared filter mode.
+			*/
+			driver->enableMaterial2D();
+			driver->draw2DImage(images, core::rect<s32>(10,10,108,48),
+				core::rect<s32>(354,87,442,118));
+			driver->enableMaterial2D(false);
+
+			/*
+			Finally draw a half-transparent rect under the mouse cursor.
+			*/
+			core::position2d<s32> m = device->getCursorControl()->getPosition();
+			driver->draw2DRectangle(video::SColor(100,255,255,255),
+				core::rect<s32>(m.X-20, m.Y-20, m.X+20, m.Y+20));
+
+			driver->endScene();
+		}
 	}
-	IAnimatedMeshSceneNode* node = smgr->addAnimatedMeshSceneNode( mesh );
 
-	/*
-	To let the mesh look a little bit nicer, we change its material. We
-	disable lighting because we do not have a dynamic light in here, and
-	the mesh would be totally black otherwise. Then we set the frame loop,
-	such that the predefined STAND animation is used. And last, we apply a
-	texture to the mesh. Without it the mesh would be drawn using only a
-	color.
-	*/
-	if (node)
-	{
-		node->setMaterialFlag(EMF_ANTI_ALIASING, true);
-		node->setMD2Animation(scene::EMAT_STAND);
-		node->setMaterialTexture( 0, driver->getTexture("./media/sydney.bmp") );
-	}
-
-	/*
-	To look at the mesh, we place a camera into 3d space at the position
-	(0, 30, -40). The camera looks from there to (0,5,0), which is
-	approximately the place where our md2 model is.
-	*/
-	smgr->addCameraSceneNode(0, vector3df(0,30,-40), vector3df(0,5,0));
-
-	/*
-	Ok, now we have set up the scene, lets draw everything: We run the
-	device in a while() loop, until the device does not want to run any
-	more. This would be when the user closes the window or presses ALT+F4
-	(or whatever keycode closes a window).
-	*/
-	while (device->run())
-	{
-		/*
-		Anything can be drawn between a beginScene() and an endScene()
-		call. The beginScene() call clears the screen with a color and
-		the depth buffer, if desired. Then we let the Scene Manager and
-		the GUI Environment draw their content. With the endScene()
-		call everything is presented on the screen.
-		*/
-		//const SExposedVideoData& videoData = video::SExposedVideoData();
-		//rect<irr::s32>* myRect = new rect<irr::s32>(0, 0, 2500, 2000);
-		//driver->beginScene(true, true, SColor(255,100,101,140),videoData,myRect);
-		driver->beginScene(true, true, SColor(255, 100, 101, 140));
-
-		smgr->drawAll();
-		guienv->drawAll();
-
-		driver->endScene();
-	}
-
-	/*
-	After we are done with the render loop, we have to delete the Irrlicht
-	Device created before with createDevice(). In the Irrlicht Engine, you
-	have to delete all objects you created with a method or function which
-	starts with 'create'. The object is simply deleted by calling ->drop().
-	See the documentation at irr::IReferenceCounted::drop() for more
-	information.
-	*/
 	device->drop();
 
 	return 0;
 }
 
 /*
-That's it. Compile and run.
+That's all. I hope it was not too difficult.
 **/
